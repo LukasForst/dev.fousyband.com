22619cfb7ad099911c705867157714e14a4ce6d7
Lukas Forst <lukas@forst.pw>
2020-09-08 12:41:44 +0200

    display just number

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,77 @@
 <template>
   <div id="app">
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="//fonts.googleapis.com/css?family=Long Cang:300,400,500,700,400italic" rel="stylesheet">
-    <Container/>
+    <div id="name">
+      <p>FOUsY</p>
+    </div>
+    <div id="num-tag">
+      <p>#{{ days + 1 }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 
-import Container from "@/components/Container";
-
 export default {
   name: 'App',
-  components: {Container}
+  data() {
+    return {
+      finalReleaseDate: new Date(2020, 8, 9, 21, 0, 0),
+      days: 0,
+    }
+  },
+  methods: {
+    updateTimer() {
+      let delta = Math.abs(this.finalReleaseDate - new Date()) / 1000;
+
+      if (delta <= 0) {
+        this.days = 0
+        this.hours = 0
+        this.minutes = 0
+        this.seconds = 0
+        return
+      }
+
+      this.days = Math.floor(delta / 86400);
+
+      setTimeout(() => this.updateTimer(), 1000)
+    }
+  },
+  created() {
+    this.updateTimer()
+  }
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 @font-face {
   font-family: "Phorssa";
   src: local("Phorssa"),
   url(./fonts/Phorssa.ttf) format("truetype");
+}
+
+
+#app {
+  position: absolute;
+  margin: auto;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+}
+
+#name {
+  font-size: 7rem;
+  font-family: Phorssa, serif;
+  color: black;
+}
+
+#num-tag > p {
+  font-family: Phorssa, serif;
+  font-size: 7rem;
+  color: black;
 }
 </style>
